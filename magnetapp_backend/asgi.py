a2ff347bef95d3116be36d34d829c5b2a9b11062
sajith231 <sajiththomas231@gmail.com>
2026-06-03 16:27:58 +0530

    chat setup

--- magnetapp_backend/asgi.py
+++ magnetapp_backend/asgi.py
@@ -1,13 +1,18 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+# Initialize Django settings and apps before importing routing
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magnetapp_backend.settings')
+django.setup()
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magnetapp_backend.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
